0b40c36252128d72831b8a9c5e250388ac8a67d8
Marc-André Bélanger <marc-andre.belanger@asciimail.com>
2022-03-12 21:38:05 -0500

    Add half-rung representation.

--- viewer/draw.js
+++ viewer/draw.js
@@ -30,10 +30,17 @@ const bases = (() => {
 })();
 
 const elementColors = {
-  C: "0x808080", // gray
-  N: "0x0000ff", // blue
-  O: "0xff0000", // red
-  P: "0xffa500", // orange
+  C: 0x808080, // gray
+  N: 0x0000ff, // blue
+  O: 0xff0000, // red
+  P: 0xffa500, // orange
+}
+
+const baseColors = {
+  A: 0xff0000, // red
+  G: 0xff00ff, // fuchsia
+  C: 0x00ff00, // lime
+  U: 0xffff00, // yellow
 }
 
 // Atom is an object with cartesian coordinates
@@ -47,16 +54,16 @@ function sphereFromAtom(atom, r=0.5) {
 }
 
 // Phosphorus backbone
-function drawBackbone(atoms, w) {
+function drawBackbone(atoms, atm, color=0xffa500, w=5) {
   let points = [];
   molecule.atoms.forEach(a => {
-    if (a.element === "P") {
+    if (a.name === atm) {
       points.push(new THREE.Vector3(a.x, a.y, a.z));
     }
   })
 
   let m = new THREE.LineBasicMaterial({
-    color: 0xffa500, // orange
+    color: color, // orange by default
     linewidth: w
   });
   let g = new THREE.BufferGeometry().setFromPoints(points);
@@ -90,6 +97,7 @@ function drawBonds(base, w) {
 const drawingOrder = {
   G: [
     ["C1'", "N9", "C8", "N7", "C5", "C6", "N1", "C2", "N3", "C4", "N9"],
+    ["C4", "C5"],
     ["C6", "O6"],
     ["C2", "N2"],
     // Ribose
@@ -100,6 +108,7 @@ const drawingOrder = {
   ],
   A: [
     ["C1'", "N9", "C8", "N7", "C5", "C6", "N1", "C2", "N3", "C4", "N9"],
+    ["C4", "C5"],
     ["C6", "N6"],
     // Ribose
     ["P", "C5'", "C4'", "C3'", "C2'", "C1'"],
@@ -136,6 +145,33 @@ function drawSpheres(atoms, w=0.5) {
   });
 }
 
-drawBackbone(molecule.atoms, 5);
-bases.forEach(b => drawBonds(b, 2));
+function drawHalfRung(base, w=5) {
+  let t = base.type;
+  let c3p = base.atoms["C3'"];
+
+  var points = [new THREE.Vector3(c3p.x, c3p.y, c3p.z)];
+
+  if (t === "A" || t === "G") { // Purines
+    // C3' -> N1
+    let n1 = base.atoms["N1"];
+    points.push(new THREE.Vector3(n1.x, n1.y, n1.z));
+  } else { // Pyrimidines
+    // C3' -> N3
+    let n3 = base.atoms["N3"];
+    points.push(new THREE.Vector3(n3.x, n3.y, n3.z));
+  }
+
+  let m = new THREE.LineBasicMaterial({
+    color: baseColors[t],
+    linewidth: w
+  });
+  let g = new THREE.BufferGeometry().setFromPoints(points);
+  let l = new THREE.Line(g, m);
+
+  scene.add(l);
+}
+
+drawBackbone(molecule.atoms, "C3'");
+bases.forEach(b => drawHalfRung(b));
+// bases.forEach(b => drawBonds(b, 2));
 // drawSpheres(molecule.atoms, 0.5);
